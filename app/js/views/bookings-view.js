08d7c23ad046cdc4cd4752c838b56ff8e80b02f1
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2015-08-24 22:48:46 -0500

    Added click event to add a new resource

--- app/js/views/bookings-view.js
+++ app/js/views/bookings-view.js
@@ -9,10 +9,21 @@ var App = App || {};
 
     initialize: function() {
       this.$table = this.$('table tbody');
-      this.listenTo(this.collection, 'add', this.addResource);
+      this.listenTo(this.collection, 'add', this.addResourceToView);
     },
 
-    addResource: function(resource) {
+    events: {
+      'click #add-resource': 'addResource'
+    },
+
+    addResource: function() {
+      this.collection.add({
+        name: 'Resource',
+        bookings: [0, 0, 0, 0]
+      });
+    },
+
+    addResourceToView: function(resource) {
       var view = new App.ResourceView({
         model: resource
       })
