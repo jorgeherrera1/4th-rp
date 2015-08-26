29709fee0460a292c34a40ddb803256a9021e905
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2015-08-26 18:02:51 -0500

    Added function to calculate week endings based on the project start date

--- app/js/models/project.js
+++ app/js/models/project.js
@@ -8,6 +8,17 @@ var App = App || {};
     defaults: {
       name: 'Project',
       startDate: new Date()
+    },
+
+    calculateWeekEndings: function(numberOfWeeks) {
+      var weekEndingsRange = _.range(numberOfWeeks);
+      var startDate = moment(this.get('startDate'));
+
+      var weekEndings = _.map(weekEndingsRange, function(weekNumber) {
+        return startDate.add(weekNumber, 'weeks').endOf('week').format('MM/DD/YYYY');
+      });
+
+      return weekEndings;
     }
 
   });
