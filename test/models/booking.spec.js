9e92490d6f17bf6f3a7f33ff9615541466778647
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2015-09-24 20:44:51 -0500

    Added tests for booking model

--- test/models/booking.spec.js
+++ test/models/booking.spec.js
@@ -10,4 +10,25 @@ describe('Booking Model', function() {
     expect(booking.get('hours')).toBe(0);
   });
 
+  it('should be invalid if hours are non numeric', function() {
+    booking.set({hours: '8h'}, {validate:true});
+
+    expect(booking.validationError).not.toBeNull();
+    expect(booking.validationError).toBe('Booking must be a number');
+  });
+
+  it('should be invalid if hours are negative', function() {
+    booking.set({hours: -1}, {validate:true});
+
+    expect(booking.validationError).not.toBeNull();
+    expect(booking.validationError).toBe('Booking cannot be negative');
+  });
+
+  it('should be valid if hours are positive', function() {
+    booking.set({hours: 8}, {validate:true});
+
+    expect(booking.validationError).toBeNull();
+    expect(booking.get('hours')).toBe(8);
+  });
+
 });
