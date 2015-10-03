e322e03c1ebba20af6f8a2208b2175727c201bb8
Jorge Herrera <jorge.herrera.cabrera@gmail.com>
2015-10-03 11:15:13 -0500

    Added test to validate bookings are in order

--- test/collections/bookings.spec.js
+++ test/collections/bookings.spec.js
@@ -1,7 +1,19 @@
 describe('Bookings Collection', function() {
 
-  beforeEach(function() {
+  var bookings;
 
+  beforeEach(function() {
+    bookings = new App.Bookings();
+  });
+
+  it('should order the bookings by date', function() {
+    bookings.add({date: new Date(2015, 5, 15)});
+    bookings.add({date: new Date(2013, 5, 10)});
+    bookings.add({date: new Date(2011, 5, 5)});
+
+    expect(bookings.at(0).get('date')).toEqual(new Date(2011, 5, 5));
+    expect(bookings.at(1).get('date')).toEqual(new Date(2013, 5, 10));
+    expect(bookings.at(2).get('date')).toEqual(new Date(2015, 5, 15));
   });
 
 });
